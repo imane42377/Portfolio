c5a4a3db0033bca7f14a97098f16ef1d6de0764b
Meliane Imane <imanemeliane18@gmail.com>
2025-11-16 21:48:22 +0100

    Update layout.js

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -31,6 +31,9 @@ export default async function LocaleLayout({ children, params }) {
   const messages = (await import(`../../../messages/${locale}.json`)).default;
   return (
     <html lang={locale}>
+    <head>
+    <link rel="icon" href="/favicon.png" sizes="any" />
+  </head>
       <body className={`${jetbrainsMono.variable}`}>
         <NextIntlClientProvider locale={locale} messages={messages}>
           <Header />
